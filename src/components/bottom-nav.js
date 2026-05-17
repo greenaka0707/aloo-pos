41b626defaa84012295de184254f1a30f2222b0f
greenaka0707 <produkpebeka@gmail.com>
2026-05-17 16:39:06 +0700

    Create bottom-nav.js

---
+++ src/components/bottom-nav.js
@@ -0,0 +1,27 @@
+export function BottomNav() {
+  return `
+    <nav class="bottom-nav">
+
+      <button class="nav-item active">
+        Dashboard
+      </button>
+
+      <button class="nav-item">
+        Order
+      </button>
+
+      <button class="nav-item">
+        Produksi
+      </button>
+
+      <button class="nav-item">
+        Stok
+      </button>
+
+      <button class="nav-item">
+        Akun
+      </button>
+
+    </nav>
+  `;
+}
