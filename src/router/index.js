512e40e46c96201249a9eac81a901f0130b6ab2b
greenaka0707 <produkpebeka@gmail.com>
2026-05-17 19:19:51 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { DashboardPage } from "../pages/dashboard";
 import { OrderListPage } from "../pages/order-list-page";
+import { OrderDetailPage } from "../pages/order-detail-page";
 
 export function renderRoute(route) {
+
   switch (route) {
 
     case "order":
       return OrderListPage();
+
+    case "order-detail":
+      return OrderDetailPage();
 
     case "dashboard":
     default:
