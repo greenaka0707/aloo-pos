cbba365c714f1c323a6c66cec164b961e2ebe561
greenaka0707 <produkpebeka@gmail.com>
2026-05-17 18:46:25 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import { AppLayout } from "./layouts/app-layout";
-import { DashboardPage } from "./pages/dashboard";
 
-document.querySelector("#app").innerHTML =
-  AppLayout(
-    DashboardPage()
-  );
+import { BottomNav } from "./components/bottom-nav";
 
-lucide.createIcons();
+import { renderRoute } from "./router";
+
+function renderPage(route = "dashboard") {
+
+  document.querySelector("#app").innerHTML =
+    AppLayout(
+      renderRoute(route)
+    );
+
+  document.querySelector("#bottom-nav").innerHTML =
+    BottomNav(route);
+
+  lucide.createIcons();
+}
+
+/* =====================
+   GLOBAL NAVIGATE
+===================== */
+
+window.navigate = function(route) {
+  renderPage(route);
+};
+
+/* =====================
+   INITIAL
+===================== */
+
+renderPage();
