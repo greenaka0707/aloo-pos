6f275e036f28c1d18836841f5cc3d393ed8c00dd
greenaka0707 <produkpebeka@gmail.com>
2026-05-17 16:39:34 +0700

    Create dashboard.js

---
+++ src/pages/dashboard.js
@@ -0,0 +1,15 @@
+export function DashboardPage() {
+  return `
+    <section class="dashboard-page">
+
+      <h1>
+        ENNA POS
+      </h1>
+
+      <p>
+        Mobile Manufacturing POS
+      </p>
+
+    </section>
+  `;
+}
