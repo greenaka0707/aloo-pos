ba97151e6a5b1a4a70ec33c382c7f6b3dfa7c8a7
greenaka0707 <produkpebeka@gmail.com>
2026-05-18 15:34:55 +0700

    Create stock-page.js

---
+++ src/pages/stock-page.js
@@ -0,0 +1,23 @@
+export function StockPage() {
+  return `
+    <section class="page">
+
+      <div class="page-header">
+
+        <div>
+
+          <h2 class="text-3xl font-bold">
+            Stock
+          </h2>
+
+          <p class="text-light text-md">
+            Inventory management
+          </p>
+
+        </div>
+
+      </div>
+
+    </section>
+  `;
+}
