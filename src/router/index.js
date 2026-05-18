824903c1bb8493b67732aaa3db02b0c4b6a2a751
greenaka0707 <produkpebeka@gmail.com>
2026-05-18 16:16:28 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { DashboardPage } from "../pages/dashboard.js";
+
 import { OrderListPage } from "../pages/order-list-page.js";
 import { OrderDetailPage } from "../pages/order-detail-page.js";
 import { CreateOrderPage } from "../pages/create-order-page.js";
+
 import { ProduksiListPage } from "../pages/produksi-list-page.js";
+import { ProduksiDetailPage } from "../pages/produksi-detail-page.js";
+
 import { StockPage } from "../pages/stock-page.js";
 
 export function renderRoute(route) {
@@ -20,16 +24,17 @@ export function renderRoute(route) {
 
     case "create-order":
       return CreateOrderPage();
-      
+
     case "produksi":
       return ProduksiListPage();
-    
+
+    case "produksi-detail":
+      return ProduksiDetailPage();
+
     case "stok":
       return StockPage();
 
     default:
       return DashboardPage();
-      
-      
   }
 }
